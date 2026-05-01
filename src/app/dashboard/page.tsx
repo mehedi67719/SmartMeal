7acd25b57dc68017174d5bceb359a9ba4dd453da
Mehedi Hassan <meh67719@gamil.com>
2026-05-01 21:11:26 +0600

    dashboard page make dynamic

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,43 @@
-
+"use client"
 import AdminDashboard from '@/component/pages/Dashboard/Admindashboard/admindashboard';
 import Managerdashboard from '@/component/pages/Dashboard/Managerdashboard/managerdashboard';
 import Memberdashboard from '@/component/pages/Dashboard/Memberdashboard/Memberdashboard';
+import { useSession } from 'next-auth/react';
 import React from 'react';
 
-const page = () => {
+const Page = () => {
+  const { data: session, status } = useSession();
+  const role = session?.user?.accountType;
+
+  console.log(role);
+  console.log(status); 
+
+
+  if (status === "loading") {
+    return (
+      <div style={{ 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        height: '100vh' 
+      }}>
+        <div>Loading...</div>
+      </div>
+    );
+  }
+
+
+  if (status === "unauthenticated") {
+    return <div>Please login to access this page</div>;
+  }
+
   return (
     <div>
-      {/* <Managerdashboard/> */}
-      {/* <Memberdashboard/> */}
-
-      <AdminDashboard/>
+      {role === "member" && <Memberdashboard />}
+      {role === "controller" && <AdminDashboard />}
+      {role === "manager" && <Managerdashboard />}
     </div>
   );
 };
 
-export default page;
+export default Page;
