79769eb85d112225aecbcd50524ca9c67ee89fa5
Mehedi Hassan <meh67719@gamil.com>
2026-05-04 23:54:07 +0600

    create health-ai page

---
+++ src/app/health-ai/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='contaienr'>
+            The work on this page is in progress
+        </div>
+    );
+};
+
+export default page;
