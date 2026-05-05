9a17c2c576501cdd24f4d99f08243c51d2fc64d7
Mehedi Hassan <meh67719@gamil.com>
2026-05-05 09:48:37 +0600

    solve warning

--- src/app/health-ai/page.tsx
+++ src/app/health-ai/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div className='contaienr'>
+        <div className='container'>
             The work on this page is in progress
         </div>
     );
